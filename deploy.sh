5b004f10715c698aeee85feac83e2262dc18a852
Luke Spragg <luke@spragg.me>
2015-01-07 23:12:53 -0500

    Add deployment of snapshots via Travis-CI

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+function die_with() { echo "$*" >&2; exit 1; }
+
+echo "Checking if commit is a pull request"
+if [ $TRAVIS_PULL_REQUEST == true ]; then die_with "Skipping deployment for pull request!"; fi
+
+echo "Configuring git credentials"
+git config --global user.email "travis@travis-ci.org" && git config --global user.name "Travis" || die_with "Failed to configure git credentials!"
+
+echo "Changing directory to $HOME and configuring git"
+cd $HOME || die_with "Failed to change to home directory!"
+
+echo "Cloning snapshots branch using token"
+git clone -q --branch=snapshots https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG.git snapshots >/dev/null || die_with "Failed to clone existing snapshots branch!"
+
+echo "Copying target files to temp directory"
+mkdir -p $HOME/temp/RustDedicated_Data/Managed || die_with "Failed to create RustDedicated_Data/Managed directories!"
+cd $HOME/build/$TRAVIS_REPO_SLUG || die_with "Failed to change to build directory!"
+cp -f RustExperimental.opj OxidePatcher/bin/Release/OxidePatcher.exe $HOME/temp || die_with "Failed to copy RustExperimental.opj and OxidePatcher.exe!"
+cp -f Dependencies/ICSharpCode.TextEditor.dll Dependencies/LinqBridge.dll Dependencies/Mono.Cecil.dll Dependencies/Newtonsoft.Json.dll $HOME/temp || die_with "Failed to copy dependency DLLs!"
+
+echo "Archiving and compressing target files"
+cd $HOME/temp || die_with "Failed to change to temp directory!"
+zip -vr9 $HOME/snapshots/Oxide2Patcher.zip . || die_with "Failed to archive snapshot files!"
+
+echo "Adding, committing, and pushing to snapshots branch"
+cd $HOME/snapshots || die_with "Failed to change to snapshots directory!"
+git add -f . && git commit -m "Oxide 2 Patcher build $TRAVIS_BUILD_NUMBER" || die_with "Failed to add and commit files with git!"
+git push -qf origin snapshots >/dev/null || die_with "Failed to push snapshot to GitHub!"
+
+echo "Deployment cycle completed. Happy developing!"
