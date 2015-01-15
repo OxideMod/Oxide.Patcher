4c28e970160bdd25ead75c12d722d0186639d8f8
Luke Spragg <luke@spragg.me>
2015-01-15 17:50:51 -0500

    Re-add temp directory creation in deploy script

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,7 @@ echo "Cloning snapshots branch using token"
 git clone -q --branch=snapshots https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG.git snapshots >/dev/null || die_with "Failed to clone existing snapshots branch!"
 
 echo "Copying target files to temp directory"
+mkdir -p $HOME/temp || die_with "Failed to create temp directory!"
 cd $HOME/build/$TRAVIS_REPO_SLUG || die_with "Failed to change to build directory!"
 cp -f RustExperimental.opj OxidePatcher/bin/Release/OxidePatcher.exe $HOME/temp || die_with "Failed to copy RustExperimental.opj and OxidePatcher.exe!"
 cp -f Dependencies/ICSharpCode.TextEditor.dll Dependencies/LinqBridge.dll Dependencies/Mono.Cecil.dll Dependencies/Newtonsoft.Json.dll $HOME/temp || die_with "Failed to copy dependency DLLs!"
