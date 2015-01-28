639f5d81ef5f6d6d325a0719ab680c68cc35b4de
Luke Spragg <luke@spragg.me>
2015-01-27 19:39:08 -0500

    Just a little bit of cleanup and changes

--- deploy.sh
+++ deploy.sh
@@ -8,24 +8,17 @@ if [ $TRAVIS_PULL_REQUEST == true ]; then die_with "Skipping deployment for pull
 echo "Configuring git credentials"
 git config --global user.email "travis@travis-ci.org" && git config --global user.name "Travis" || die_with "Failed to configure git credentials!"
 
-echo "Changing directory to $HOME and configuring git"
-cd $HOME || die_with "Failed to change to home directory!"
-
 echo "Cloning snapshots branch using token"
-git clone -q --branch=snapshots https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG.git snapshots >/dev/null || die_with "Failed to clone existing snapshots branch!"
+git clone -q --branch=snapshots https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG.git $HOME/snapshots >/dev/null || die_with "Failed to clone existing snapshots branch!"
 
-echo "Copying target files to temp directory"
-mkdir $TRAVIS_COMMIT || die_with "Failed to create temp directory!"
-cd build/$TRAVIS_REPO_SLUG || die_with "Failed to change to build directory!"
-cp -vf RustExperimental.opj OxidePatcher/bin/Release/OxidePatcher.exe $HOME/$TRAVIS_COMMIT || die_with "Failed to copy RustExperimental.opj and OxidePatcher.exe!"
+cd $HOME/build/$TRAVIS_REPO_SLUG || die_with "Failed to change to build directory!"
 
-echo "Archiving and compressing target files"
-cd $HOME/$TRAVIS_COMMIT || die_with "Failed to change to temp directory!"
-zip -vFS9 $HOME/snapshots/OxidePatcher.zip * || die_with "Failed to archive snapshot files!"
+echo "Copying target files to snapshots directory"
+cp -f OxidePatcher/bin/Release/OxidePatcher.exe $HOME/snapshots || die_with "Failed to copy OxidePatcher.exe to snapshots!"
 
 echo "Adding, committing, and pushing to snapshots branch"
 cd $HOME/snapshots || die_with "Failed to change to snapshots directory!"
-git add -f . && git commit -m "Oxide 2 Patcher build $TRAVIS_BUILD_NUMBER" || die_with "Failed to add and commit files with git!"
+git add -f . && git commit -m "Oxide Patcher build $TRAVIS_BUILD_NUMBER" || die_with "Failed to add and commit files with git!"
 git push -qf origin snapshots >/dev/null || die_with "Failed to push snapshot to GitHub!"
 
 echo "Deployment cycle completed. Happy developing!"
