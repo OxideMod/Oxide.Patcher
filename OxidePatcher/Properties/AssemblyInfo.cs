8ddc30fb6c79cfd3468ec4f6ccca8bbdc22e7b57
Luke Spragg <luke@spragg.me>
2015-09-21 13:54:11 -0400

    Update AssemblyInfo [ci skip]

--- OxidePatcher/Properties/AssemblyInfo.cs
+++ OxidePatcher/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -8,9 +7,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Oxide Patcher")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Oxide Team")]
+[assembly: AssemblyCompany("Oxide")]
 [assembly: AssemblyProduct("Oxide Patcher")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Oxide 2014-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
